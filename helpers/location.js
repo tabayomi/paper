a97a7723a3f2cf406cf38d537d10da935518acd7
Tolu Abayomi <tolu.abayomi@bigcommerce.com>
2017-03-22 15:42:40 -0700

    output safestring html

--- helpers/location.js
+++ helpers/location.js
@@ -4,6 +4,8 @@ function helper(paper) {
     paper.handlebars.registerHelper('location', function (locationId) {
         const options = arguments[arguments.length - 1];
 
-        return `Output for location ${locationId}`;
+        return new paper.handlebars.SafeString(paper.context.locations[locationId]) || '';
     });
 }
+
+module.exports = helper;
