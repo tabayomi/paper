36646302b1b854bde9df1e96c319ffaf6b6aeb55
Tolu Abayomi <tolu.abayomi@bigcommerce.com>
2017-03-23 14:19:10 -0700

    fix typo in location helper

--- helpers/location.js
+++ helpers/location.js
@@ -4,10 +4,10 @@ function helper(paper) {
     paper.handlebars.registerHelper('location', function (locationId) {
         const options = arguments[arguments.length - 1];
         
-        if ((!paper.contexts) || (!paper.context.locations)) {
+        if ((!paper.context) || (!paper.context.locations)) {
             return '';
         }
-        
+
         return new paper.handlebars.SafeString(paper.context.locations[locationId]) || '';
     });
 }
